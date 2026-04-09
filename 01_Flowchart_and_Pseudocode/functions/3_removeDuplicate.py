e9c2c6f6e80402c61045b367cf776ecbcfb193f8
Vinod3d <vinodchandra979@gmail.com>
2026-04-09 12:21:04 +0530

    basic string and array questions

--- 01_Flowchart_and_Pseudocode/functions/3_removeDuplicate.py
+++
@@ -1,9 +0,0 @@
-def removeDuplicate(arr):
-    unique_arr = []
-    for num in arr:
-        if num not in unique_arr:
-            unique_arr.append(num)
-    return unique_arr
-
-arr = [1, 2, 2, 3, 4, 4, 5]
-print(removeDuplicate(arr))
